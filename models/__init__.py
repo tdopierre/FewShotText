31fa627dfe860c7e8c7d9d5507801aebf87327aa
Thomas Dopierre <thomas.dopierre@protonmail.com>
2020-07-02 10:48:07 +0200

    Ignores jupyter checkpoints

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from .encoders.bert_encoder import BERTEncoder
+from .bert_baseline.baseline import BaselineNet
+from .induction.inductionnet import InductionNet
+from .matching.matchingnet import MatchingNet
+from .proto.protonet import ProtoNet
+from .relation.relationnet import RelationNet

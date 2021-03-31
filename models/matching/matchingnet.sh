895d5807a02daf574ee6bca46a3d857bfa29bad5
Thomas Dopierre <thomas.dopierre@protonmail.com>
2021-03-31 08:41:26 +0200

    Adds missing SH scripts to run each model

---
+++ models/matching/matchingnet.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+cd $HOME/Projects/FewShotText
+source .venv/bin/activate
+source .envrc
+echo "CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}"
+command -v nvidia-smi >/dev/null && {
+    echo "GPU Devices:"
+    nvidia-smi
+} || {
+    :
+}
+
+PYTHONPATH=. python models/matching/matchingnet.py $@
